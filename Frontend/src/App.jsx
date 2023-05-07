56b92058ae39c8d2674eb2da73c2610ee3c97228
Hesara Pasan <hesaramahendra123@gmail.com>
2023-05-07 09:22:50 +0530

    add feedback form create

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Complaints from "./pages/complaints";
+import Feedback from "./pages/Addfeedback";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/test" element={<Complaints />} />
+          <Route path="/feedback" element={<Feedback/>}/>
         </Routes>
       </BrowserRouter>
     </>
