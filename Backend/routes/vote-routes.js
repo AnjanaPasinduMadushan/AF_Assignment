2e4686b96f99d5701c586595d65ee4f185bfbdb9
IT21021534-FernandoKJP <IT21021534@my.sliit.lk>
2023-05-17 21:08:32 +0530

    checkVote backend

--- Backend/routes/vote-routes.js
+++ Backend/routes/vote-routes.js
@@ -3,5 +3,6 @@ const voteController = require("../controllers/voteController");
 const middleware = require("../middlewares/user-middleware")
 
 router.post("/add", middleware.checkToken, middleware.citizenRole, voteController.vote);
+router.get("/checkVote/:complaintId", middleware.checkToken, middleware.citizenRole, voteController.checkVote);
 
 module.exports = router;
