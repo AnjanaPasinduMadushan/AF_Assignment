03de644767ba1973157c4a7884103daec6475090
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2023-05-17 22:23:09 +0530

    Minor changes in admin side

--- Frontend/src/components/users.jsx
+++ Frontend/src/components/users.jsx
@@ -8,117 +8,120 @@ import '../assets/users.css'
 const Users = () => {
 
   //  const { userData, getUserData } = useContext(AuthContext);
-    const [users, setUsers] = useState([])
-    const history = useNavigate()
+  const [users, setUsers] = useState([])
+  const history = useNavigate()
 
-   // console.log(userData)
+  // console.log(userData)
 
-    useEffect(()=>{
-       // getUserData();
-        const getUsers=async()=>{
-            
-            const res = await axios.get('http://localhost:8070/User/newUsers', {withCredentials:true}).catch((err)=>{
-                console.log(err)
-            })
-            setUsers(res.data.users)
-            console.log(res.data.users)
-        }
+  useEffect(() => {
+    // getUserData();
+    const getUsers = async () => {
 
-        getUsers();
-    }, [])
-
-    const hanldeVerifying = async(userId) => {
-        try{
-
-            const res = await axios.patch(`http://localhost:8070/User/verifyUser/${userId}`,{withCredentials:true}, {
-                checkingIn:true
-            })
-            const newUpdateUser = res.data;
-
-            setUsers((prevUsers) => {
-              const newUsers = [...prevUsers];
-      
-              const arrIndex = newUsers.findIndex(
-                (user) => user._id === newUpdateUser._id
-              );
-              newUsers[arrIndex] = newUpdateUser;
-      
-              console.log(newUsers)
-              return newUsers;
-            });
-        }catch(err){
-            console.log(err)
-        }
+      const res = await axios.get('http://localhost:8070/User/newUsers', { withCredentials: true }).catch((err) => {
+        console.log(err)
+      })
+      setUsers(res.data.users)
+      console.log(res.data.users)
     }
 
+    getUsers();
+  }, [])
 
-    const handleUnverify = async(userId) =>{
-        try{
+  const hanldeVerifying = async (userId) => {
+    try {
 
-            const res = await axios.delete(`http://localhost:8070/User/UnverifyUser/${userId}`)
-            
-            const newUpdateUser = res.data;
+      const res = await axios.patch(`http://localhost:8070/User/verifyUser/${userId}`, { checkingIn: true },
+        { withCredentials: true })
 
-            setUsers((prevUsers) => {
-              const newUsers = [...prevUsers];
-      
-              const arrIndex = newUsers.findIndex(
-                (user) => user._id === newUpdateUser._id
-              );
-              newUsers[arrIndex] = newUpdateUser;
-      
-              console.log(newUsers)
-              return newUsers;
-            })
+      const newUsers = res.data;
+      console.log(res.data)
+      setUsers((prevUsers) =>
 
-        }catch(err){
-            console.log(err)
-        }
+        prevUsers.filter((user) => user._id !== userId),
+
+
+        //{
+        //   const newUsers = [...prevUsers];
+
+        //   const arrIndex = newUsers.findIndex(
+        //     (user) => user._id === newUpdateUser._id
+        //   );
+        //   newUsers[arrIndex] = newUpdateUser;
+
+        //   console.log(newUsers)
+        //   return newUsers;
+        // }
+
+
+      );
+
+
+    } catch (err) {
+      console.log(err)
     }
+  }
+
+
+  const handleUnverify = async (userId) => {
+    try {
+
+      const res = await axios.delete(`http://localhost:8070/User/UnverifyUser/${userId}`)
+
+      //const newUpdateUser = res.data;
+
+      setUsers((prevUsers) =>
+        prevUsers.filter((user) => user._id !== userId)
+
+      );
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <div>
 
-      
-        <br/>
 
-       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-  <button onClick={() => history(`/currentUsers`)} className='custome_btn'>Current Users</button>
+      <br />
 
-  <button onClick={() => history(`/newComplaints`)} className='custome_btn'>New Complaints</button>
-</div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <button onClick={() => history(`/currentUsers`)} className='custome_btn'>Current Users</button>
 
-<h1><center>NEW ACCOUNT CREATION REQUESTS</center></h1>
-        <div>
-        {users && users.length>0 ? (
-            <table className='users_table'>
-                <thead>
-                <tr>
+        <button onClick={() => history(`/newComplaints`)} className='custome_btn'>New Complaints</button>
+      </div>
+
+      <h1><center>NEW ACCOUNT CREATION REQUESTS</center></h1>
+      <div>
+        {users && users.length > 0 ? (
+          <table className='users_table'>
+            <thead>
+              <tr>
                 <th className='tableCell' id="tableCell_td">NIC</th>
                 <th className='tableCell' id="tableCell_td">NAME</th>
                 <th className='tableCell' id="tableCell_td">EMAIL</th>
                 <th className='tableCell' id="tableCell_td">MOBILE</th>
                 <th className='tableCell' id="tableCell_td">ACTIONS</th>
-                </tr>
-                </thead>
-            
+              </tr>
+            </thead>
+
             <tbody>
-            {users.map((user, key)=>(
-            <tr  key={key}>
-                <td className='tableCell'>{user.NIC}</td>
-                <td className='tableCell'>{user.name}</td>
-                <td className='tableCell'>{user.email}</td>
-                <td className='tableCell'>{user.mobile}</td>
-                <th className='tableCell'><button onClick={()=>hanldeVerifying(user._id)} className='btn btn-warning'>Verify</button>
-                <button onClick={()=>handleUnverify(user._id)} className='btn btn-danger' style={{marginLeft: '10px'}}>Unverify</button></th>
-            </tr>
-            ))}
+              {users.map((user, key) => (
+                <tr key={key}>
+                  <td className='tableCell'>{user.NIC}</td>
+                  <td className='tableCell'>{user.name}</td>
+                  <td className='tableCell'>{user.email}</td>
+                  <td className='tableCell'>{user.mobile}</td>
+                  <th className='tableCell'><button onClick={() => hanldeVerifying(user._id)} className='btn btn-warning'>Verify</button>
+                    <button onClick={() => handleUnverify(user._id)} className='btn btn-danger' style={{ marginLeft: '10px' }}>Unverify</button></th>
+                </tr>
+              ))}
             </tbody>
-           
-                
-            </table>)
-            :(<h1 className='message'><center>There are not any users sent Account creation requests!!!</center></h1>)}
-        </div>
+
+
+          </table>)
+          : (<h1 className='message'><center>There are not any users sent Account creation requests!!!</center></h1>)}
+      </div>
     </div>
   )
 }
