f93525919eacc151625af5e34d7053094c906637
thushanvithana <87628351+thushanvithana@users.noreply.github.com>
2023-05-19 08:58:07 +0530

    delete done

--- Frontend/src/components/CreateForm.jsx
+++ Frontend/src/components/CreateForm.jsx
@@ -1,5 +1,82 @@
+// import { complaintsStore } from "../stores/complaintsStore";
+// import "../asset/CreateForm.css";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import Upload from "./Upload";
+// import swal from "sweetalert";
+
+// export default function CreateForm(props) {
+//   const [title, setTitle] = useState("");
+//   const [description, setDescription] = useState("");
+
+//   async function submit(e) {
+//     e.preventDefault();
+
+//     const data = {
+//       title,
+//       description,
+//       image: "dummy",
+//     };
+
+//     try {
+//       const complaint = await axios.post(
+//         "http://localhost:8070/complaint/complaints",
+//         data
+//       );
+//       swal("successs");
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   }
+//   // const { uploadComponent } = props;
+
+//   // const complaintStore = complaintsStore();
+
+//   // if (complaintStore.updateForm._id) return <></>;
+
+//   return (
+//     <div className="form_comaplin">
+//       <form onSubmit={submit}>
+//         <h1>
+//           <center>Create new Complaint</center>
+//         </h1>
+//         Your confidentiality will be ensured.
+//         <input
+//           type="text"
+//           onChange={(e) => setTitle(e.target.value)}
+//           value={title}
+//           name="title"
+//           placeholder="type your complaint-Title"
+//           className="complaint_text"
+//         /> Report Corruption
+//         <br />
+//         <br />  Please describe the incident *
+//         <textarea
+//           onChange={(e) => setDescription(e.target.value)}
+//           value={description}
+//           name="description"
+//           placeholder="type your complaint"
+//           className="textarea"
+//         />
+//         <div>
+//           {/* other form fields
+//           {uploadComponent}
+
+//           <Upload /> */}
+
+//           <button type="submit" className="custom-button">
+//             Submit
+//           </button>
+//         </div>
+//         <Upload/>
+//       </form>
+
+ 
+//     </div>
+//   );
+// }
 import { complaintsStore } from "../stores/complaintsStore";
-import '../asset/CreateForm.css'
+import "../asset/CreateForm.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Upload from "./Upload";
@@ -8,66 +85,96 @@ import swal from "sweetalert";
 export default function CreateForm(props) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [descriptionError, setDescriptionError] = useState("");
+  const [titleWordCount, setTitleWordCount] = useState(0);
+  const [descriptionWordCount, setDescriptionWordCount] = useState(0);
+
+  useEffect(() => {
+    setTitleWordCount(title.trim().split(" ").filter(Boolean).length);
+    setDescriptionWordCount(description.trim().split(" ").filter(Boolean).length);
+  }, [title, description]);
 
   async function submit(e) {
     e.preventDefault();
 
-    const data = {
-      title,
-      description,
-      image: "dummy"
-    }
+    if (validateForm()) {
+      const data = {
+        title,
+        description,
+        image: "dummy",
+      };
 
-    try {
-      const complaint = await axios.post("http://localhost:8070/complaint/complaints", data);
-      swal("successs");
-    } catch (e) {
-      console.log(e);
+      try {
+        const complaint = await axios.post(
+          "http://localhost:8070/complaint/complaints",
+          data
+        );
+        swal("successs");
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
-  // const { uploadComponent } = props;
 
+  function validateForm() {
+    let isValid = true;
 
-  // const complaintStore = complaintsStore();
+    if (title.trim() === "") {
+      setTitleError("Please enter a title");
+      isValid = false;
+    } else {
+      setTitleError("");
+    }
 
+    if (description.trim() === "") {
+      setDescriptionError("Please enter a description");
+      isValid = false;
+    } else {
+      setDescriptionError("");
+    }
 
-  // if (complaintStore.updateForm._id) return <></>;
+    return isValid;
+  }
 
   return (
     <div className="form_comaplin">
       <form onSubmit={submit}>
-        <h1><center>Create new Complaint</center></h1>
-
+        <h1>
+          <center>Create new Complaint</center>
+        </h1>
+        Your confidentiality will be ensured.
         <input
           type="text"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
           name="title"
-          placeholder="type your complaint-Title"
+          placeholder="Type your complaint title"
           className="complaint_text"
         />
-
+        {titleError && <p className="error">{titleError}</p>}
+        Word Count: {titleWordCount}
         <br />
+        Report Corruption
         <br />
+        <br /> Please describe the incident *
         <textarea
           onChange={(e) => setDescription(e.target.value)}
           value={description}
           name="description"
-          placeholder="type your complaint"
+          placeholder="Type your complaint"
           className="textarea"
         />
-
+        {descriptionError && <p className="error">{descriptionError}</p>}
+        Word Count: {descriptionWordCount}
         <div>
-          {/* other form fields
-          {uploadComponent}
-
-          <Upload /> */}
-
-          <button type="submit" className="custom-button">Submit</button>
+          <Upload />
+          <button type="submit" className="custom-button">
+            Submit
+          </button>
         </div>
       </form>
-
-
     </div>
   );
 }
+
